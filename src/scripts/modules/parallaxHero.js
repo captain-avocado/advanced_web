00c455f5bc0c176083a149ade5a6fd6874d040db
Artem Kaloev <kaloev.eltech@gmail.com>
2018-03-29 21:44:11 +0300

    updated parallax

--- src/scripts/modules/parallaxHero.js
+++ src/scripts/modules/parallaxHero.js
@@ -1,16 +1,19 @@
 export const parallaxHero = (function() {
     const bg = document.querySelector('.hero__bg');
     const intro = document.querySelector('.hero__intro');
-
     return {
-        move: function(block, windowScroll, strafeAmount) {
-            const strafe = windowScroll / -strafeAmount + '%';
-            // const style = block.style;
-            console.log(block);
-            block.style.top = strafe;
+        move(block, windowScroll, strafeAmount) {
+            const strafe = windowScroll / -strafeAmount;
+            const leftValue = window.getComputedStyle(block, null).getPropertyValue('left');
+            const coord = (leftValue === '0px') ? 0 : ('-50');
+            const transformString = `translate3d(${coord}%, ${strafe + coord}%, 0)`;
+            const style = block.style;
+            style.transform = transformString;
+            style.webkitTransform = transformString;
         },
-        init: function(wScroll) {
+        init(wScroll) {
             this.move(bg, wScroll, 40);
+            this.move(intro, wScroll, 3);
         },
     };
 
