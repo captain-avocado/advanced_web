be222a91ae678da0e2739f700aa37a4160949ca5
Artem Kaloev <kaloev.eltech@gmail.com>
2018-04-03 04:13:54 +0300

    fixing slider

--- src/scripts/modules/slider.js
+++ src/scripts/modules/slider.js
@@ -7,51 +7,82 @@ export const slider = (function() {
     const nextControl = document.querySelector('.slide-next');
     const prevControl = document.querySelector('.slide-prev');
 
-    const moveSlide = function(slider, counter) {
-        // let counter = 1;
+    let counter = 1;
+    let inProcess = false;
+
+    const moveSlide = function(slider, direction) {
         // console.log(counter);
 
-        
         const items = Array.from(slider.children);
         const activeItem = items.filter(item => item.classList.contains('active'))[0];
 
-        //??????
+
+        // const innerCounter = (direction < 0) ? counter - 1 : counter + 1;
+        // if (counter <= 0) {
+        //     counter = 4;
+        // }
+
+        let innerCounter = (direction < 0) ? items.length - counter : counter;
+        
+        if (innerCounter >= items.length) {
+            innerCounter = 0;
+            counter = 0;
+        }
+        console.log('innerCounter = ' + innerCounter);
+        const reqItem = items[innerCounter];
+
         // if (counter >= items.length) {
         //     counter = 0;
         // }
-        
-        const reqItem = items[counter];
+        // const reqItem = items[counter];
 
-        activeItem.style.transition = 'top 1s';
-        activeItem.style.top = '100%';
+        // if (slider === next) direction = -direction;
+        activeItem.style.transition = 'top .5s';
+        activeItem.style.top = direction * 100 + '%';
 
         activeItem.addEventListener('transitionend', () => {
             activeItem.style.transition = '';
             activeItem.style.top = '';
             activeItem.classList.remove('active');
         });
-        
-        reqItem.style.transition = 'top 1s';
+            
+        reqItem.style.transition = 'top .5s';
         reqItem.style.top = 0;
 
         reqItem.addEventListener('transitionend', () => {
             reqItem.style.transition = '';
             reqItem.classList.add('active');
+            inProcess = false;
         });
+
+
     };
  
 
 
     return {
         init() {
-            let counter = 1;
-           
+            // let counter = 1;
             prevControl.addEventListener('click', (e) => {
                 e.preventDefault();
 
-                if (counter >= prev.children.length) counter = 0;
-                moveSlide(prev, counter);
-                counter++;
+                // if (counter >= prev.children.length) counter = 0;
+                if (!inProcess) {
+                    inProcess = true;
+                    moveSlide(prev, 1);
+                    moveSlide(next, -1);
+                    counter++;
+                }
+            });
+
+            nextControl.addEventListener('click', (e) => {
+                e.preventDefault();
+                if (!inProcess) {
+                    inProcess = true;
+                    moveSlide(next, 1);
+                    moveSlide(prev, -1);
+                    counter++;
+                }
             });
             
         },
