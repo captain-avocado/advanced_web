2760bd07815fbdc318b90d1092e8ab3623cc30e8
Artem Kaloev <kaloev.eltech@gmail.com>
2018-04-03 23:16:42 +0300

    finished slider

--- src/scripts/modules/slider.js
+++ src/scripts/modules/slider.js
@@ -7,24 +7,48 @@ export const slider = (function() {
     const nextControl = document.querySelector('.slide-next');
     const prevControl = document.querySelector('.slide-prev');
 
-    let counter = 1;
+    let prevCounter = 0, nextCounter = 0, previewCounter = 0;
     let inProcess = false;
 
-    let fadeSliderCounter = 0;
+    const info = [
+        {
+            title: 'сайт школы онлайн образования',
+            tech: 'html, css, javascript',
+            href: 'https://google.com',
+        },
+        {
+            title: 'лэндинг магазина бургеров',
+            tech: 'html, scss, javascript + jQuery',
+            href: 'https://stackoverflow.com',
+        },
+        {
+            title: 'сайт кофоркинга',
+            tech: 'html, css, javascriptик',
+            href: 'https://loftschool.com',
+        },
+        {
+            title: 'последний слайд',
+            tech: 'tech',
+            href: 'https://github.com',
+        },
+    ];
 
-    const moveSlide = function(slider, blockDirection, slideDirection) {
+    const processCounter = (counter, direction) => {
+        if (direction > 0) counter++;
+        if (direction < 0) counter--;
+
+        if (counter < 0) counter = 3;
+        if (counter > 3) counter = 0;
+        return counter;
+    };
+    
+    const moveSlide = function(slider, blockDirection, slideDirection, counter) {
 
         const items = Array.from(slider.children);
-        const activeItem = items.filter(item => item.classList.contains('active'))[0];
+        const activeItem = items[counter];
+        counter = processCounter(counter, blockDirection);
+        const reqItem = items[counter];
 
-        let innerCounter = (blockDirection < 0) ? items.length - counter : counter;
-        
-        if (innerCounter >= items.length) {
-            innerCounter = 0;
-            counter = 0;
-        }
-        console.log('innerCounter = ' + innerCounter);
-        const reqItem = items[innerCounter];
 
         activeItem.style.transition = 'top .5s';
         activeItem.style.top = slideDirection * 100 + '%';
@@ -44,38 +68,54 @@ export const slider = (function() {
             inProcess = false;
         });
 
+        return counter;
     };
  
 
-    const fadeSlide = function(slider, direction) {
+    const fadeSlide = function(slider, direction, counter) {
         
         const items = Array.from(slider.children);
-        const activeItem = items.filter(item => item.classList.contains('active'))[0];
+        const activeItem = items[counter];
 
-        if (direction > 0) fadeSliderCounter++;
-        if (direction < 0) fadeSliderCounter--;
+        counter = processCounter(counter, direction);
 
-        if (fadeSliderCounter < 0) fadeSliderCounter = 3;
-        if (fadeSliderCounter > 3) fadeSliderCounter = 0;
-
-        const reqItem = items[fadeSliderCounter];
+        const reqItem = items[counter];
 
         // transitionend??
         activeItem.classList.remove('active');
         reqItem.classList.add('active');
+
+        const title = document.getElementById('show-title');
+        const tech = document.getElementById('show-tech');
+        const link = document.getElementById('show-link');
+
+        title.innerHTML = info[counter].title;
+        title.animate({
+            // keyframes
+            opacity: [0.5, 1],
+            transform: ['scale(0.8)', 'scale(1.05)', 'scale(0.9)', 'scale(1)'],
+        }, { 
+            // timing options
+            duration: 300,
+            // iterations: Infinity
+        });
+        tech.innerHTML = info[counter].tech;
+        link.href = info[counter].href;
+        return counter;
     };
 
     return {
         init() {
+            preview.children[0].classList.add('active');
+
             prevControl.addEventListener('click', (e) => {
                 e.preventDefault();
-
+                
                 if (!inProcess) {
                     inProcess = true;
-                    moveSlide(prev, 1, 1);
-                    moveSlide(next, -1, -1);
-                    fadeSlide(preview, -1);
-                    counter++;
+                    prevCounter = moveSlide(prev, 1, 1, prevCounter);
+                    nextCounter = moveSlide(next, -1, -1, nextCounter);
+                    previewCounter = fadeSlide(preview, -1, previewCounter);
                 }
             });
 
@@ -83,10 +123,9 @@ export const slider = (function() {
                 e.preventDefault();
                 if (!inProcess) {
                     inProcess = true;
-                    moveSlide(next, 1, -1);
-                    moveSlide(prev,-1, 1);
-                    fadeSlide(preview, 1);
-                    counter++;
+                    nextCounter = moveSlide(next, 1, -1, nextCounter);
+                    prevCounter =  moveSlide(prev, -1, 1, prevCounter);
+                    previewCounter = fadeSlide(preview, 1, previewCounter);
                 }
             });
             
